da0447498663aea485282f58e89da987615ab52e
Fajar Satria <ryansat46@gmail.com>
2019-01-13 16:46:46 +0700

    add show image after upload

--- application/views/Upload_success.php
+++ application/views/Upload_success.php
@@ -8,24 +8,37 @@
       <h3>Your file was successfully uploaded!</h3>  
       <?php foreach ($upload_data as $item => $value):?> 
       <?php
-
+         echo $item;
          if ($item == 'full_path')
           {
             // echo $item;
              //$data = $item;
          }
+         if ($item == 'file_name')
+         {
+            $image = $value;
+         }
 
           
           ?> <?php 
+          echo $value;
           if ($item == 'full_path')
           {
-             //echo $value;
              $data = $value;
+            }
+            if ($item == 'file_name')
+            {
+               $image = $value;
             }
           ?>
          <?php endforeach; ?>
-		<p> <?php echo $data;?> </p>
-      <p><?php echo anchor('upload', 'Upload Another File!'); ?></p>  
+      <p> <?php echo $data;?> </p>
+      <p> <?php 
+     // $images = glob($data);
+      //echo base_url('uploads/a.jpg');   
+
+      echo '<img src='.base_url('uploads/'.$image).' /><br />'; ?></p>
+      <p></p>  
    </body>
 	
 </html>
